65270423c2fba3db3457e05a4ac7ad2e191d6ca5
notsooryaa <stunsooryaa@gmail.com>
2025-12-27 22:40:35 +0530

    learnt for in ang

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,16 @@ import { Component, signal } from '@angular/core';
   styleUrl: './app.css'
 })
 export class App {
+  tasks: string[] = [];
+
+  addTask(task: string) {
+    if (task.trim()) {
+      this.tasks.push(task);
+    }
+  }
+
+  removeTask(index: number) {
+    this.tasks.splice(index, 1);
+  }
+
 }
